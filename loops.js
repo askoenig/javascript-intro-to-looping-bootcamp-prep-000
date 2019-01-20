c6e21987d809493bce2f45cd20f0f1590d3a42ff
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-01-20 02:02:27 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -24,7 +24,7 @@ function whileLoop(n) {
 function doWhileLoop(array) {
   do { array.shift()
 }
-while (array.length > 0 && incrementVariable)
+while (array.length > 0 && incrementVariable())
 return array
 
 }
